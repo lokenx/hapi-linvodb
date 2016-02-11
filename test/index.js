2da63fcad04c7d0c085216e86a4397c55db0faaa
Mikhail HASIT <mdelport@dyn129-100-123-141.has.uwo.ca>
2016-02-11 09:06:57 -0500

    Directory output before and after test added

--- test/index.js
+++ test/index.js
@@ -51,12 +51,16 @@ lab.experiment('Plugin Tests', () => {
       }
     }, (err) => {
 
-      const files = FS.readdirSync(Path.join(__dirname, '..'));
-      console.info('\n Files in dir:\n', files);
+      const files_before = FS.readdirSync(Path.join(__dirname, '..'));
+      console.info('\n Files in dir before:\n', files_before);
 
       const db = Path.join(__dirname, '..', 'testlinvodb.1');
       expect(err).to.not.exist();
       expect(FS.accessSync(db, FS.F_OK)).to.not.exist();
+
+      const files_after = FS.readdirSync(Path.join(__dirname, '..'));
+      console.info('\n Files in dir before:\n', files_after);
+
       done();
     });
   });

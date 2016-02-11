d58be9be51ca960bc9ae1e9a1b8e7eb255c1fe33
Mikhail Delport <mikhail@delport.ca>
2016-02-10 19:48:00 -0500

    Initial plugin and tests

--- readme.md
+++ readme.md
@@ -1,3 +1,71 @@
-# hapi-linvodb
+# hapi-linvodb [![Build Status](https://travis-ci.org/lokenx/hapi-linvodb.svg?branch=master)](https://travis-ci.org/lokenx/hapi-linvodb) [![codecov.io](https://codecov.io/github/lokenx/hapi-linvodb/coverage.svg?branch=master)](https://codecov.io/github/lokenx/hapi-linvodb?branch=master)
 
 Simple [Hapi](http://hapijs.com/) plugin for [linvodb3](https://github.com/Ivshti/linvodb3)
+
+# Installation
+
+```
+npm install hapi-linvodb
+```
+
+# Usage
+
+```
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+server.register({
+    register: require('hapi-linvodb'),
+    options: {
+      modelName: 'test',
+      filename: 'testlinvo.db',
+      schema: {}
+    }
+}, (err) => {
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, reply) => {
+
+            // Access plugin
+            var db = server.plugins['hapi-linvodb'].db;
+
+            // Use requests database and create requests database if it does not exist
+            db.insert({ a: 1 }, (err, doc) => {
+
+                reply({ message: 'request added to database' });
+            });
+
+            // If using a schema you have to create a new object otherwise it's ignored
+            const user = new db({});
+            user.name = 'Adam';
+
+            user.save(() => {
+
+              reply({ message: 'request added to database' });
+          });
+
+        }
+    });
+});
+
+// Start the server
+server.start(() => {
+
+    console.info(`Hapi times at ${server.info.uri}`);
+});
+```
+
+# License
+
+MIT 2016
---
+++ test/index.js
@@ -0,0 +1,187 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Lab = require('lab');
+const Code = require('code');
+const FS = require('fs');
+const Trash = require('trash');
+
+const internals = {};
+
+const lab = exports.lab = Lab.script();
+const expect = Code.expect;
+
+lab.experiment('Plugin Tests', () => {
+
+  let server = {};
+
+  lab.beforeEach((done) => {
+
+    server = new Hapi.Server();
+    done();
+  });
+
+  lab.test('Rejection of invalid options', (done) => {
+
+    const register = () => {
+
+      server.register({
+        register: require('../'),
+        options: {
+          directory: 'blah'
+        }
+      }, (err) => {
+
+        throw err;
+      });
+    };
+
+    expect(register).to.throw(Error);
+    done();
+  });
+
+  lab.test('Successful registration of plugin', (done) => {
+
+    server.register({
+      register: require('../'),
+      options: {
+        modelName: 'test',
+        filename: 'testlinvo.db',
+        schema: {}
+      }
+    }, (err) => {
+
+      expect(err).to.not.exist();
+      expect(FS.accessSync('./testlinvo.db', FS.F_OK)).to.not.exist();
+      done();
+    });
+  });
+
+  lab.test('Successfully access plugin exposed objects and methods', (done) => {
+
+    server.connection();
+    server.register({
+      register: require('../'),
+      options: {
+        modelName: 'test',
+        filename: 'testlinvo.db',
+        schema: {}
+      }
+    }, (err) => {
+
+      expect(err).to.not.exist();
+
+      server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, reply) => {
+
+          const plugin = request.server.plugins['hapi-linvodb'];
+
+          expect(plugin.db.modelName).to.equal('test');
+          expect(plugin.db.filename).to.equal('testlinvo.db');
+
+          plugin.db.insert({ a: 1 }, (err, doc) => {
+
+            expect(err).to.not.exist();
+            expect(doc.a).to.equal(1);
+
+            plugin.db.find({}, (err, docs) => {
+
+              expect(err).to.not.exist();
+              expect(docs).to.be.an.array();
+              done();
+            });
+          });
+        }
+      });
+
+      server.inject({
+        method: 'GET',
+        url: '/'
+      }, (res) => {
+
+      });
+
+    });
+  });
+
+  lab.test('Successful schema creation', (done) => {
+
+    server.connection();
+    server.register({
+      register: require('../'),
+      options: {
+        modelName: 'test',
+        filename: 'testlinvo.db',
+        schema: {
+          name: {
+            type: String,
+            default: 'nameless'
+          },
+          age: Number
+        }
+      }
+    }, (err) => {
+
+      expect(err).to.not.exist();
+
+      server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, reply) => {
+
+          // Get plugin and create a new object
+          const plugin = request.server.plugins['hapi-linvodb'];
+          const user = new plugin.db({ age: 18 });
+
+          // Confirm passed variable and default get set
+          expect(user.age).to.equal(18);
+          expect(user.name).to.equal('nameless');
+
+          user.name = 'Adam';
+          user.save((err) => {
+
+            expect(err).to.not.exist();
+
+            plugin.db.findOne({ name: 'Adam' }, (err, changed_user) => {
+
+              // Confirm we can retrieve and change saved object
+              expect(err).to.not.exist();
+              expect(changed_user.name).to.equal('Adam');
+
+              changed_user.age = 'Age';
+              changed_user.save((err) => {
+
+                expect(err).to.not.exist();
+
+                plugin.db.findOne({ name: 'Adam' }, (err, unchanged_user) => {
+
+                  // Confirm schema is working. It doesn't generate errors, it just won't make changes if they
+                  // don't match the schema
+                  expect(err).to.not.exist();
+                  expect(unchanged_user.age).to.equal(18);
+                  done();
+                });
+              });
+            });
+          });
+        }
+      });
+
+      server.inject({
+        method: 'GET',
+        url: '/'
+      }, (res) => {
+
+      });
+    });
+
+  });
+
+  lab.afterEach((done) => {
+
+    Trash(['./testlinvo.db']).then(done());
+  });
+
+});
